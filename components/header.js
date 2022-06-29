9b81a7b928afbffa149fcf5d98266627d5c74acb
oktay <oktaycolakoglu@gmail.com>
2022-06-29 23:27:10 +0300

    Remove Header CSS file

--- components/header.js
+++ components/header.js
@@ -1,11 +1,9 @@
 import Navigation from '@comp/navigation';
 import Email from '@comp/email';
-import styles from '@styl/header.module.css';
-import cx from 'classnames';
 
 export default function Header() {
   return (
-    <header className={cx(styles.header, 'hidden lg:block')}>
+    <header className="py-8 absolute w-full top-0 left-0 hidden lg:block">
       <div className="container-fluid">
         <div className="flex flex-col justify-between items-center md:flex-row">
           <Email />
