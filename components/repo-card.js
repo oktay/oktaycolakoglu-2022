75812a720c0c6d02bb7cb6dbe92fa0705e6b1cf3
oktay <oktaycolakoglu@gmail.com>
2022-06-30 01:47:47 +0300

    Repo Card Improvements

--- components/repo-card.js
+++ components/repo-card.js
@@ -1,4 +1,9 @@
-import { BsStar, BsEye, BsFileEarmarkCode } from 'react-icons/bs';
+import {
+  BsStar,
+  BsEye,
+  // BsFileEarmarkCode,
+  BsLink45Deg,
+} from 'react-icons/bs';
 
 export default function RepoCard({
   id,
@@ -6,9 +11,11 @@ export default function RepoCard({
   description,
   stargazers_count,
   watchers_count,
-  language,
+  // language,
   html_url,
   fork,
+  homepage,
+  topics,
 }) {
   function onClick() {
     const analyticsData = {
@@ -20,39 +27,78 @@ export default function RepoCard({
 
     window.dataLayer.push(analyticsData);
   }
+
+  function onHomepageClick() {
+    const analyticsData = {
+      event: 'Click',
+      action: 'Repository Homepage Click',
+      target: 'Github Section Repository Card',
+      label: homepage,
+    };
+
+    window.dataLayer.push(analyticsData);
+  }
   return (
     <div
       key={id}
       className="relative group h-full w-3/4 sm:w-full flex-shrink-0"
     >
       <div className="aspect-3/2">
-        <a
-          href={html_url}
-          target="_blank"
-          rel="noreferrer"
-          onClick={onClick}
-          className="card flex flex-col h-full w-full"
-        >
-          <div className="font-semibold text-lg flex items-center space-x-2">
+        <div className="card flex flex-col h-full w-full">
+          <a
+            href={html_url}
+            target="_blank"
+            rel="noreferrer"
+            onClick={onClick}
+            className="font-semibold lg:text-lg flex items-center space-x-2"
+          >
             <span className="truncate">{name}</span>
             {fork && <span className="badge">FORK</span>}
-          </div>
-          <p className="text-sm mt-2 text-zinc-500 truncate">{description}</p>
-          <div className="flex space-x-2 mt-auto">
-            <div className="badge bg-white">
+          </a>
+          <p className="text-sm mt-2 text-zinc-500 truncate">
+            {description || 'No description'}
+          </p>
+          {topics.length > 0 && (
+            <div className="hidden lg:flex flex-wrap gap-2 mt-4">
+              {topics.map((topic) => (
+                <span
+                  key={topic}
+                  className="badge text-xs bg-teal-100 text-teal-700 border-teal-200"
+                >
+                  {topic}
+                </span>
+              ))}
+            </div>
+          )}
+          <div className="flex items-center gap-2 mt-auto">
+            {/* <div className="text-xs inline-flex items-center gap-1">
               <BsFileEarmarkCode />
               <span>{language || 'Mixed'}</span>
-            </div>
-            <div className="badge bg-white">
+            </div> */}
+            <div className="text-xs inline-flex items-center gap-1">
               <BsStar />
               <span>{stargazers_count}</span>
             </div>
-            <div className="badge bg-white">
+            <div className="text-xs inline-flex items-center gap-1">
               <BsEye />
               <span>{watchers_count}</span>
             </div>
+            {homepage && (
+              <a
+                href={homepage}
+                onClick={onHomepageClick}
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center ml-auto whitespace-nowrap text-ellipsis overflow-hidden"
+              >
+                <BsLink45Deg className="text-xl flex-shrink-0" />
+                <span className="text-teal-600 font-semibold text-xs whitespace-nowrap text-ellipsis overflow-hidden">
+                  {homepage}
+                </span>
+              </a>
+            )}
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
