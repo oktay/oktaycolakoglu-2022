c8e546badaaa5712458bfe5bad581877918c2471
Oktay <oktaycolakoglu@gmail.com>
2022-07-18 15:18:09 +0300

    Refactor locale switch (#27)

--- components/locale-switch.js
+++ components/locale-switch.js
@@ -11,27 +11,35 @@ export default function LocaleSwitch() {
   return (
     <Link href={route} locale={locale === 'tr' ? 'en' : 'tr'}>
       <a className="w-full bg-white text-black p-1 pl-2 rounded-full text-xs tracking-widest font-bold inline-flex items-center group">
-        <HiTranslate className="text-lg mr-2" />
+        <HiTranslate className="text-lg mr-2 lg:group-hover:text-teal-400 transition" />
         <span className="mr-auto uppercase lg:sr-only">{t('lang')}</span>
-        <span
-          className={cx(
-            locale === 'tr'
-              ? 'bg-black text-white group-hover:bg-transparent group-hover:text-black'
-              : 'group-hover:bg-black group-hover:text-white',
-            'rounded-full px-2 transition-all',
-          )}
-        >
-          TR
-        </span>
-        <span
-          className={cx(
-            locale === 'en'
-              ? 'bg-black text-white group-hover:bg-transparent group-hover:text-black'
-              : 'group-hover:bg-black group-hover:text-white',
-            'rounded-full px-2 transition-all',
-          )}
-        >
-          EN
+        <span className="relative">
+          <span
+            className={cx(
+              'relative px-2 z-10 transition',
+              locale === 'tr'
+                ? 'text-white'
+                : 'text-black lg:group-hover:text-teal-400',
+            )}
+          >
+            TR
+          </span>
+          <span
+            className={cx(
+              'relative px-2 z-10 transition',
+              locale === 'en'
+                ? 'text-white'
+                : 'text-black lg:group-hover:text-teal-400',
+            )}
+          >
+            EN
+          </span>
+          <span
+            className={cx(
+              'bg-teal-400 absolute w-1/2 h-full inset-0 rounded-full transition-all',
+              locale === 'en' ? 'left-1/2' : 'left-0',
+            )}
+          ></span>
         </span>
       </a>
     </Link>
