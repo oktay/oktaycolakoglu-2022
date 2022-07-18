5cce2aa0c79e00aca3e13406c17dcd53b37fa797
oktay <oktaycolakoglu@gmail.com>
2022-07-18 16:19:29 +0300

    Remove monochrome icon

--- pages/index.js
+++ pages/index.js
@@ -43,12 +43,11 @@ export default function Home({ repos, shots, homepage, seo }) {
           content="PJVvsin3qa5Ny-g9ZEY2T_D5HmfkG1IftdDqq41TH_g"
         />
         <meta name="msapplication-TileColor" content="#2dd4bf" />
-        <meta name="theme-color" content="#2dd4bf" />
+        <meta name="theme-color" content="#0d0e10" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
         <link rel="manifest" href="/site.webmanifest?v=1" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#2dd4bf" />
         <link rel="shortcut icon" href="/favicon.ico?v=1" />
         <link rel="canonical" href={meta.url} />
 
