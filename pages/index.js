41cfb1150c1ee95ed55f6af7f292b2adc915a655
oktay <oktaycolakoglu@gmail.com>
2022-07-18 16:10:20 +0300

    Improve favicons

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,14 @@ export default function Home({ repos, shots, homepage, seo }) {
           name="google-site-verification"
           content="PJVvsin3qa5Ny-g9ZEY2T_D5HmfkG1IftdDqq41TH_g"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#2dd4bf" />
+        <meta name="theme-color" content="#2dd4bf" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#2dd4bf" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
         <link rel="canonical" href={meta.url} />
 
         {/* facebook */}
