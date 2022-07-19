738820fbbb461cb66d171f61af1775a7114e8b49
oktay <oktaycolakoglu@gmail.com>
2022-07-19 22:59:28 +0300

    Fix styles import

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '@fontsource/plus-jakarta-sans/500.css';
 import '@fontsource/plus-jakarta-sans/600.css';
 import '@fontsource/plus-jakarta-sans/700.css';
 import '@fontsource/plus-jakarta-sans/800.css';
-import '@styl/globals.css';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
