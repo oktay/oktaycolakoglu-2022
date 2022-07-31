46ae03b73fc0d193fa73b6bca60d7ede780a10d8
oktay <oktaycolakoglu@gmail.com>
2022-08-01 00:04:25 +0300

    Refactor data file

--- pages/api/dribbble.js
+++ pages/api/dribbble.js
@@ -4,32 +4,30 @@ export default function handler(_req, res) {
     return
   }
 
-  res.status(200).json({
-    shots: [
-      {
-        id: 1,
-        images: {
-          two_x: '/images/shot-1.jpg',
-        },
-        title: 'Example Shot',
-        html_url: 'https://dribbble.com/oktaycolakoglu',
+  res.status(200).json([
+    {
+      id: 1,
+      images: {
+        two_x: '/images/shot-1.jpg',
       },
-      {
-        id: 2,
-        images: {
-          two_x: '/images/shot-2.jpg',
-        },
-        title: 'Example Shot',
-        html_url: 'https://dribbble.com/oktaycolakoglu',
+      title: 'Example Shot',
+      html_url: 'https://dribbble.com/oktaycolakoglu',
+    },
+    {
+      id: 2,
+      images: {
+        two_x: '/images/shot-2.jpg',
       },
-      {
-        id: 3,
-        images: {
-          two_x: '/images/shot-3.jpg',
-        },
-        title: 'Example Shot',
-        html_url: 'https://dribbble.com/oktaycolakoglu',
+      title: 'Example Shot',
+      html_url: 'https://dribbble.com/oktaycolakoglu',
+    },
+    {
+      id: 3,
+      images: {
+        two_x: '/images/shot-3.jpg',
       },
-    ],
-  })
+      title: 'Example Shot',
+      html_url: 'https://dribbble.com/oktaycolakoglu',
+    },
+  ])
 }
--- pages/api/github.js
+++ pages/api/github.js
@@ -4,38 +4,36 @@ export default function handler(_req, res) {
     return
   }
 
-  res.status(200).json({
-    repos: [
-      {
-        id: 1,
-        name: 'example-repo',
-        html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
-        description: 'Example repo description',
-        fork: false,
-        stargazers_count: 0,
-        watchers_count: 0,
-        language: 'JavaScript',
-      },
-      {
-        id: 2,
-        name: 'example-fork',
-        html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
-        description: 'Example fork repo description',
-        fork: true,
-        stargazers_count: 0,
-        watchers_count: 0,
-        language: 'JavaScript',
-      },
-      {
-        id: 3,
-        name: 'example-repo',
-        html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
-        description: 'Example repo description',
-        fork: false,
-        stargazers_count: 0,
-        watchers_count: 0,
-        language: 'JavaScript',
-      },
-    ],
-  })
+  res.status(200).json([
+    {
+      id: 1,
+      name: 'example-repo',
+      html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
+      description: 'Example repo description',
+      fork: false,
+      stargazers_count: 0,
+      watchers_count: 0,
+      language: 'JavaScript',
+    },
+    {
+      id: 2,
+      name: 'example-fork',
+      html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
+      description: 'Example fork repo description',
+      fork: true,
+      stargazers_count: 0,
+      watchers_count: 0,
+      language: 'JavaScript',
+    },
+    {
+      id: 3,
+      name: 'example-repo',
+      html_url: 'https://github.com/oktay/oktaycolakoglu-2022',
+      description: 'Example repo description',
+      fork: false,
+      stargazers_count: 0,
+      watchers_count: 0,
+      language: 'JavaScript',
+    },
+  ])
 }
